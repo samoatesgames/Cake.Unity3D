6b9e76ba6f1ca94696bdb0bc6ed19b342952d96d
samoatesgames <sam@samoatesgames.com>
2018-05-03 10:23:15 +0100

    Move the unity3d log interaction methods into a static helper class.

---
+++ src/Cake.Unity3D/Helpers/Unity3DEditor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cake.Unity3D.Helpers
+{
+    /// <summary>
+    /// Some helper methods for interacting with Unity3D.
+    /// </summary>
+    public static class Unity3DEditor
+    {
+        /// <summary>
+        /// Gets the absolute path to the Unity3D editor log.
+        /// This presumes that Unity3D is running as a user and not a service.
+        /// </summary>
+        /// <returns>The absolute path to the Unity3D editor log.</returns>
+        public static string GetEditorLogLocation()
+        {
+            var localAppdata = Environment.GetEnvironmentVariable("LocalAppData");
+            return System.IO.Path.Combine(localAppdata, "Unity", "Editor", "Editor.log");
+        }
+
+        /// <summary>
+        /// Output all new log lines to the console for the specified log.
+        /// </summary>
+        /// <param name="logLocation">The location of the log file to redirect.</param>
+        /// <param name="currentLine">The line of the log of which we have already redirected.</param>
+        /// <returns></returns>
+        public static int OutputLogToConsole(string logLocation, int currentLine)
+        {
+            // The log doesn't exist, so we can't output its contents
+            // to the console.
+            if (!System.IO.File.Exists(logLocation))
+            {
+                return currentLine;
+            }
+
+            // Read all lines from the log.
+            var lines = SafeReadAllLines(logLocation);
+
+            // If the log output is less that what we have already
+            // redirected, return what we have outputted presuming
+            // something went wrong when reading the file.
+            if (lines.Length <= currentLine)
+            {
+                return currentLine;
+            }
+
+            // Output all new lines of the log.
+            foreach (var line in lines.Skip(currentLine))
+            {
+                Console.WriteLine(line);
+            }
+
+            // Return the new number of lines we have redirected.
+            return lines.Length;
+        }
+
+        /// <summary>
+        /// Read all lines from a given file.
+        /// Using read/write file share access.
+        /// </summary>
+        /// <param name="path">The absolute path to the file to read.</param>
+        /// <returns>An array of all lines in the log file.</returns>
+        private static string[] SafeReadAllLines(string path)
+        {
+            try
+            {
+                using (var fileStream = new System.IO.FileStream(path, System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.ReadWrite))
+                {
+                    using (var reader = new System.IO.StreamReader(fileStream))
+                    {
+                        var file = new List<string>();
+                        while (!reader.EndOfStream)
+                        {
+                            file.Add(reader.ReadLine());
+                        }
+                        return file.ToArray();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // Something went wrong, return an empty array
+                return new string[0];
+            }
+        }
+    }
+}
--- src/Cake.Unity3D/Unity3DBuildContext.cs
+++ src/Cake.Unity3D/Unity3DBuildContext.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using Cake.Core;
 using Cake.Core.IO;
+using Cake.Unity3D.Helpers;
 
 namespace Cake.Unity3D
 {
@@ -105,7 +106,7 @@ namespace Cake.Unity3D
             // the remaining lines. This works because Unity flushes in full lines, so we should
             // always have a full line to output.
             var outputLineIndex = 0;
-            var logLocation = LocatedEditorLog();
+            var logLocation = Unity3DEditor.GetEditorLogLocation();
 
             // Start the process.
             process.Start();
@@ -118,85 +119,8 @@ namespace Cake.Unity3D
 
                 if (m_buildOptions.OutputEditorLog)
                 {
-                    outputLineIndex = OutputLogToConsole(logLocation, outputLineIndex);
+                    outputLineIndex = Unity3DEditor.OutputLogToConsole(logLocation, outputLineIndex);
                 }
-            }
-        }
-
-        /// <summary>
-        /// Gets the absolute path to the Unity3D editor log.
-        /// This presumes that Unity3D is running as a user and not a service.
-        /// </summary>
-        /// <returns>The absolute path to the Unity3D editor log.</returns>
-        private string LocatedEditorLog()
-        {
-            var localAppdata = Environment.GetEnvironmentVariable("LocalAppData");
-            return System.IO.Path.Combine(localAppdata, "Unity", "Editor", "Editor.log");
-        }
-
-        /// <summary>
-        /// Output all new log lines to the console for the specified log.
-        /// </summary>
-        /// <param name="logLocation">The location of the log file to redirect.</param>
-        /// <param name="currentLine">The line of the log of which we have already redirected.</param>
-        /// <returns></returns>
-        private int OutputLogToConsole(string logLocation, int currentLine)
-        {
-            // The log doesn't exist, so we can't output its contents
-            // to the console.
-            if (!System.IO.File.Exists(logLocation))
-            {
-                return currentLine;
-            }
-
-            // Read all lines from the log.
-            var lines = SafeReadAllLines(logLocation);
-
-            // If the log output is less that what we have already
-            // redirected, return what we have outputted presuming
-            // something went wrong when reading the file.
-            if (lines.Length <= currentLine)
-            {
-                return currentLine;
-            }
-
-            // Output all new lines of the log.
-            foreach (var line in lines.Skip(currentLine))
-            {
-                Console.WriteLine(line);
-            }
-
-            // Return the new number of lines we have redirected.
-            return lines.Length;
-        }
-
-        /// <summary>
-        /// Read all lines from a given file.
-        /// Using read/write file share access.
-        /// </summary>
-        /// <param name="path">The absolute path to the file to read.</param>
-        /// <returns>An array of all lines in the log file.</returns>
-        private static string[] SafeReadAllLines(string path)
-        {
-            try
-            {
-                using (var fileStream = new System.IO.FileStream(path, System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.ReadWrite))
-                {
-                    using (var reader = new System.IO.StreamReader(fileStream))
-                    {
-                        var file = new List<string>();
-                        while (!reader.EndOfStream)
-                        {
-                            file.Add(reader.ReadLine());
-                        }
-                        return file.ToArray();
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                // Something went wrong, return an empty array
-                return new string[0];
             }
         }
 
