4a816f38e47b1de71bc208fdb4324757388abc12
samoatesgames <sam@samoatesgames.com>
2018-05-06 10:10:33 +0100

    Use regex patterns to parse the log output.

--- src/Cake.Unity3D/Helpers/Unity3DEditorLog.cs
+++ src/Cake.Unity3D/Helpers/Unity3DEditorLog.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
 namespace Cake.Unity3D.Helpers
 {
     /// <summary>
@@ -33,6 +36,28 @@ namespace Cake.Unity3D.Helpers
         }
 
         /// <summary>
+        /// A map between Regex patterns and the type of message they represent.
+        /// This allows us to do some basic pattern matching on log lines to improve
+        /// the readability.
+        /// </summary>
+        private static readonly Dictionary<Regex, MessageType> LogPatterns = new Dictionary<Regex, MessageType>();
+
+        /// <summary>
+        /// Static constructor used for setting up message type patterns.
+        /// </summary>
+        static Unity3DEditorLog()
+        {
+            LogPatterns[new Regex(@"build\sfailed", RegexOptions.IgnoreCase)] = MessageType.Error;
+            LogPatterns[new Regex(@"error\s\w+\d+\:", RegexOptions.IgnoreCase)] = MessageType.Error;
+
+            LogPatterns[new Regex(@"^warning\s", RegexOptions.IgnoreCase)] = MessageType.Warning;
+            LogPatterns[new Regex(@"\swarning\s\w+\d+\:", RegexOptions.IgnoreCase)] = MessageType.Warning;
+
+            LogPatterns[new Regex(@"\[Cake\.Unity3D\]", RegexOptions.IgnoreCase)] = MessageType.Info;
+            LogPatterns[new Regex(@"^-\s", RegexOptions.IgnoreCase)] = MessageType.Info;
+        }
+
+        /// <summary>
         /// Given a line of text from the editor log, calculate
         /// the type of message the line is.
         /// This is very privative and needs vast improvements.
@@ -41,19 +66,12 @@ namespace Cake.Unity3D.Helpers
         /// <returns>The type of message the line represents.</returns>
         public static MessageType ProcessLogLine(string line)
         {
-            if (line.Contains("Build Failed"))
+            foreach (var pattern in LogPatterns)
             {
-                return MessageType.Error;
-            }
-
-            if (line.StartsWith("WARNING", System.StringComparison.OrdinalIgnoreCase))
-            {
-                return MessageType.Warning;
-            }
-
-            if (line.StartsWith("[Cake.Unity3D]", System.StringComparison.OrdinalIgnoreCase))
-            {
-                return MessageType.Info;
+                if (pattern.Key.IsMatch(line))
+                {
+                    return pattern.Value;
+                }
             }
 
             return MessageType.Debug;
