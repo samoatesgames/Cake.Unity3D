09c51dd4d5d0135f3710593b0a71d9ccf09b49c1
Christian Taylor <christianjtaylor@sky.com>
2023-12-25 09:21:37 +0000

    Support more build platforms
    (Not complete atm - enough to unblock me)
    
    Also, ignore mac junk files

--- src/Cake.Unity3D/Build/Unity3DBuildPlatform.cs
+++ src/Cake.Unity3D/Build/Unity3DBuildPlatform.cs
@@ -6,18 +6,48 @@
     public enum Unity3DBuildPlatform
     {
         /// <summary>
-        /// Build a standalone windows x64 build.
-        /// </summary>
-        StandaloneWindows64,
-
-        /// <summary>
         /// Build a standalone windows x86 build.
         /// </summary>
-        StandaloneWindows,
+        StandaloneWindows = 5,
+
+        /// <summary>
+        /// Build a standalone windows x64 build.
+        /// </summary>
+        StandaloneWindows64 = 19,
+
+        /// <summary>
+        /// Build a standalone OSX build.
+        /// </summary>
+        StandaloneOSX = 2,
+ 
+        /// <summary>
+        /// Build a standalone iOS build.
+        /// </summary>
+        iOS = 9,
+        
+        /// <summary>
+        /// Build a standalone tvOS build.
+        /// </summary>
+        tvOS = 37,
+
+        /// <summary>
+        /// Build a standalone Linux x64 build.
+        /// </summary>
+        StandaloneLinux64 = 24,
+        
+        /// <summary>
+        /// Build a standalone Linux Headless build.
+        /// </summary>
+        LinuxHeadlessSimulation = 41,
+        
+        /// <summary>
+        /// Build a standalone Android build.
+        /// </summary>
+        Android = 13,
 
         /// <summary>
         /// Build a WebGL build.
         /// </summary>
-        WebGL
+        WebGL = 20,
     }
 }
